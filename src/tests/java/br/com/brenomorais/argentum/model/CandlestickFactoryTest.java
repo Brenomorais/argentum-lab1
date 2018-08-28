3f29ca052a539bdf4d070fe25edef21302651563
breno morais <breno.lopes.morais@hotmail>
2018-08-28 01:26:27 -0300

    adicionando test junit candlestickfactory

--- src/tests/java/br/com/brenomorais/argentum/model/CandlestickFactoryTest.java
+++ src/tests/java/br/com/brenomorais/argentum/model/CandlestickFactoryTest.java
@@ -1,6 +1,7 @@
 package br.com.brenomorais.argentum.model;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,21 +25,68 @@ class CandlestickFactoryTest {
 		CandlestickFactory fabrica = new CandlestickFactory();
 
 		Candlestick candle = fabrica.geraCandleParaData(negociacoes, hoje);
+
+		// minimo
+		Assert.assertEquals(20.0, candle.getMinimo(), 0.000001);
+
+		// maximo
+		Assert.assertEquals(45.0, candle.getMaximo(), 0.000001);
+
+		// abertura
+		Assert.assertEquals(40.0, candle.getAbertura(), 0.000001);
+
+		// fechamento
+		Assert.assertEquals(20.0, candle.getFechamento(), 0.000001);
+
+		// volume
+		Assert.assertEquals(14000.0, candle.getVolume(), 0.000001);
+	}
+
+	@Test
+	public void geraCandlestickComApenasUmaNegociacao() {
+
+		LocalDateTime data = LocalDateTime.now();
+
+		Negociacao negociacao = new Negociacao(40.0, 100, data);
+
+		List<Negociacao> negociacoes = Arrays.asList(negociacao);
+
+		CandlestickFactory fabrica = new CandlestickFactory();
+
+		Candlestick candle = fabrica.geraCandleParaData(negociacoes, data);
+
+		// minimo
+		Assert.assertEquals(40.0, candle.getMinimo(), 0.000001);
+
+		// maximo
+		Assert.assertEquals(40.0, candle.getMaximo(), 0.000001);
+
+		// abertura
+		Assert.assertEquals(40.0, candle.getAbertura(), 0.000001);
+
+		// fechamento
+		Assert.assertEquals(40.0, candle.getFechamento(), 0.000001);
+
+		// volume
+		Assert.assertEquals(4000.0, candle.getVolume(), 0.000001);
+	}
+
+	@Test
+	public void geraCandlestickComZerosEmCasoDeNenhumaNegociacao() {
+
+		LocalDateTime data = LocalDateTime.now();
+
+		List<Negociacao> negociacoes = new ArrayList<>();
+
+		CandlestickFactory fabrica = new CandlestickFactory();
+
+		Candlestick candle = fabrica.geraCandleParaData(negociacoes, data);
 		
-		//minimo
-		Assert.assertEquals(20.0, candle.getMinimo(),0.000001);
-		
-		//maximo
-		Assert.assertEquals(45.0, candle.getMaximo(),0.000001);
-		
-		//abertura
-		Assert.assertEquals(40.0, candle.getAbertura(),0.000001);
-		
-		//fechamento
-		Assert.assertEquals(20.0, candle.getFechamento(),0.000001);
-		
-		//volume
-		Assert.assertEquals(14000.0, candle.getVolume(),0.000001);
+		Assert.assertEquals(0.0, candle.getMinimo(), 0.000001);		
+		Assert.assertEquals(0.0, candle.getMaximo(), 0.000001);		
+		Assert.assertEquals(0.0, candle.getAbertura(), 0.000001);		
+		Assert.assertEquals(0.0, candle.getFechamento(), 0.000001);
+		Assert.assertEquals(0.0, candle.getVolume(), 0.000001);
 
 	}
 
